f19d16ac5ff0b826079324edfd3230b745d40cfa
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-13 20:52:48 +0300

    defining addPatient function to add new patient entry and exporting it

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -1,8 +1,7 @@
 import diagnosisData from '../../data/diagnoses';
 import patientData from '../../data/patients';
-import { Diagnosis,NonSensitiveData } from '../types';
-
-
+import { Diagnosis,NonSensitiveData,NewPatient,Patient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getDiagnosisData=():Diagnosis[]=>{
 return diagnosisData;
@@ -18,7 +17,18 @@ const getPatientData=():NonSensitiveData[]=>{
    }));
 };
 
+const addPatient=(newPatientEntry:NewPatient):Patient=>{
+  const patient:Patient={
+    id:uuid(),
+    ...newPatientEntry
+  };
+  patientData.push(patient);
+  return patient;
+};
+
+
 export default {
   getDiagnosisData,
-  getPatientData
+  getPatientData,
+  addPatient
 };
