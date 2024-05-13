2cb3e12e7161a46c8b5712f1524be6fe0805f58d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-13 20:48:39 +0300

    adding post handling router and using getNewPatiententry to give the post data NewPatient type

--- src/routes/patientRouter.ts
+++ src/routes/patientRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import patientService from '../services/patientService';
+import getNewPatientEntry from '../utils';
 
 
 const patientRouter=express.Router();
@@ -8,4 +9,21 @@ patientRouter.get('/',(_req,res)=>{
  res.send(patientService.getPatientData());
 });
 
+patientRouter.post('/',(req,res)=>{
+  try {
+    const newPatientEntry=getNewPatientEntry(req.body);
+    const patientEntry=patientService.addPatient(newPatientEntry);
+    res.json(patientEntry);
+  } catch (error:unknown) {
+    let errorMessage='something wrong:';
+    if(error instanceof Error){
+      errorMessage +=error.message;
+    }
+    res.sendStatus(400).send(errorMessage);
+  }
+ 
+});
+
 export default patientRouter;
+
+
