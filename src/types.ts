c0f68889f84780a86e6817129db6153c1cfd7b72
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-07-09 20:23:00 +0300

    adding type BaseEntry and extending it to create three other type needed for entries typing

--- src/types.ts
+++ src/types.ts
@@ -6,15 +6,50 @@ export interface Diagnosis{
 }
 
 export enum Gender{
-  male='male',
-  female='female' ,
-   other='other'
+  Male='male',
+  Female='female' ,
+  Other='other'
 } 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 
-export interface Entry{
-
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry{
+  type:"Hospital";
+  discharge:{
+    date:string;
+    criteria:string;
+  }
+}
+interface OccupationalHealthcareEntry extends BaseEntry{
+  type:"OccupationalHealthcare";
+  employerName:string;
+  sickLeave?:{
+    startDate:string;
+    endDate:string;
+  }
+}
+
+export type Entry=HealthCheckEntry | HospitalEntry | OccupationalHealthcareEntry;
+
 export interface Patient {
   id:string;
   name:string;
